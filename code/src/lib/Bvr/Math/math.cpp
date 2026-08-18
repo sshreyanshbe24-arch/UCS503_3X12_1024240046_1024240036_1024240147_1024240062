1b08ac26ed231cfe04c26f8da78ce2bce9724299
sshreyanshbe24-arch <sshreyansh_be24@thapar.edu>
2026-08-18 16:08:07 +0530

    Add files via upload

---
+++ code/src/lib/Bvr/Math/math.cpp
@@ -0,0 +1,15 @@
+#include <Bvr/Math/math.hpp>
+
+namespace Bvr {
+  namespace Math{
+
+    int sum(int a, int b) {
+      int c;
+
+      c = a + b;
+  
+      return c;
+    }
+ 
+  } //Math
+} //Bvr
